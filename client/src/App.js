14e324aa52036c39d91ff98531ca050e72d19a7e
Caitlin Collins <caitlin.aileen86@gmail.com>
2021-05-08 20:24:33 -0500

    Page routes up and running

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Saved from "./pages/saved";
+import Search from "./pages/search";
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+
+
 
 function App() {
     return (
-        <h1>Hi there!</h1>
+        <Router>
+            <div>
+                <Header />
+                <NavBar />
+                <Switch>
+                    <Route exact path={["/", "/search"]}>
+                        <Search />
+                    </Route>
+                    <Route exact path='/saved'>
+                        <Saved />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     )
 }
 
---
+++ client/src/components/NavBar/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function NavBar() {
+    return (
+        <h1> I am a NAVBAR!</h1>
+    )
+}
+
+export default NavBar;
