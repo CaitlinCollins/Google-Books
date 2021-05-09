8f1685a91a73a7fe8cfe3abfe026ee09cd5f4afe
Caitlin Collins <caitlin.aileen86@gmail.com>
2021-05-08 19:48:11 -0500

    Fixed multiple errors with file structure.

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-    return 
+    return (
+        <h1>Hi there!</h1>
+    )
 }
 
 export default App;
