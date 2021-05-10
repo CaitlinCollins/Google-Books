fab840b7054c6a08d83eabba0f3ad5f5831c6809
Caitlin Collins <caitlin.aileen86@gmail.com>
2021-05-10 12:45:07 -0500

    Added searchBooks API call. Completed API.js

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Jumbotron from "../components/Jumbotron";
 import Searches from "../components/Searches";
 import Results from "../components/Results";
+import API from "../utils/API";
+
 
 function Search() {
+    const [books, setBooks] = useState([]);
+    const [query, setQuery] = useState("");
+
+    searchBooks = query => {
+    API.search(query)
+    .then(res => {
+        const bookData = res.data.items;
+        const displayData = bookData.map(book => {
+            return {
+                id: book.id,
+                title: book.volumeInfo.title,
+                author: book.volumeInfo.authors,
+                discription: book.volumeInfo.description,
+                image: book.volumeInfo.imageLinks.thumbnail,
+                link: book.volumeInfo.infoLink
+            }
+        })
+        setBooks(displayData)
+    })
+    .catch(err => console.log(err))
+    }
+
+    handleClick = event => {
+        event.preventDefault();
+        if (query) {
+            searchBooks();
+        }
+    }
+
     return (
         <div>
             <Jumbotron />
             <Searches />
-            <Results />
+            <Results 
+            />
         </div>
     )
 }
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,5 +5,23 @@ const APIKEY = "&key=AIzaSyBzCjVzUeAXPiMvfyuxrJfE1OY37FNiD68";
 export default {
     search: function(query) {
         return axios.get(URL + query + APIKEY)
+    },
+
+    getAll: function() {
+        return axios.get("/api/books");
+    },
+
+    getOne: function() {
+        return axios.get("/api/books/" + id)
+    },
+
+    delete: function() {
+        return axios.delete("/api/books" + id)
+    },
+
+    save: function(book) {
+        return axios.post("/api/books", book)
     }
+
 };
+
